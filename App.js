7f9895f9712bb0e0a1b24db7add1502be78f32c4
vishaldole <vishaldole.1998@gmail.com>
2024-02-16 20:21:06 +0530

    episode-03

--- App.js
+++ App.js
@@ -72,6 +72,9 @@
  *
  */
 
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const Parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "This is h1 tag"),
